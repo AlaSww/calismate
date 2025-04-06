8d9a2ee8494ee759d013a97bf40712cb4ad31c01
ala <hell86486@gmail.com>
2025-04-06 19:55:45 -0400

    home page

--- lib/pages/mainPage.dart
+++ lib/pages/mainPage.dart
@@ -11,9 +11,27 @@ class Mainpage extends StatefulWidget {
 class _MainpageState extends State<Mainpage> {
   @override
   Widget build(BuildContext context) {
+    double screenwidth = MediaQuery.sizeOf(context).width;
     return Scaffold(
       appBar: AppBar(
-
+        automaticallyImplyLeading: false,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset(
+              "assets/logo.png",
+              height: screenwidth*0.3,
+              ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){}, 
+            icon:Icon(Icons.person),
+            iconSize: screenwidth*0.08,
+            )
+        ],
       ),
       bottomNavigationBar: GNav(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -34,8 +52,8 @@ class _MainpageState extends State<Mainpage> {
             gap: 8,
           ),
           GButton(
-            icon:Icons.person,
-            text: "Profile",
+            icon:Icons.settings,
+            text: "Settings",
             gap: 8,
           ),
         ],
--- lib/theme.dart
+++ lib/theme.dart
@@ -4,7 +4,7 @@ ThemeData lightmode=ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
     background: Colors.white,
-    primary:  Color.fromARGB(255, 33, 33, 33),
+    primary:  Color.fromARGB(255, 222, 222, 222),
 
   )
 );
@@ -12,6 +12,6 @@ ThemeData darkmode=ThemeData(
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
     background:  Color.fromARGB(255, 36, 36, 36),
-    secondary:  Color.fromARGB(255, 86, 86, 86)
+    primary:  Color.fromARGB(255, 86, 86, 86)
   )
 );
